b61646073662e3714922546ec79a724baf3c1d77
Dan Tenenbaum <dtenenba@fhcrc.org>
2018-10-14 19:22:42 -0700

    i have run out of brain for tonight

--- app.py
+++ app.py
@@ -22,9 +22,6 @@ API = Api(APP)
 GITHUB = Github("https://api.github.com")
 
 
-# CFG = {}
-
-
 def verify_env_vars():
     "verify correct env vars are set"
     for var in ["GITHUB_TOKEN", "GITHUB_ORG", "GITHUB_TEAM"]:
@@ -32,24 +29,25 @@ def verify_env_vars():
         if not tmp:
             print("Environment variable {} not set! Exiting.".format(var))
             sys.exit(1)
-        # else:
-        #     CFG[var] = tmp
 
 
-HEADERS = {}
+def get_headers():
+    "get headers"
+    headers = {}
+    headers["Authorization"] = "token {}".format(os.getenv("GITHUB_TOKEN"))
+    headers["Accept"] = "application/vnd.github.hellcat-preview+json"
+    return headers
 
 
 def setup_headers():
     "set up headers"
-    HEADERS["Authorization"] = "token {}".format(os.getenv("GITHUB_TOKEN"))
-    HEADERS["Accept"] = "application/vnd.github.hellcat-preview+json"
 
 
 def get_paginated_results(url, delay=0.3):
     """
     Handle paginated results transparently, returning them as one list.
     """
-    results = requests.get(url, headers=HEADERS)
+    results = requests.get(url, headers=get_headers())
     if not "Link" in results.headers:
         return results.json()
     all_results = results.json()
@@ -60,7 +58,7 @@ def get_paginated_results(url, delay=0.3):
         nextpage = [x for x in links if 'rel="next"' in x][0]
         nextpage = nextpage.split(";")[0].replace("<", "").replace(">", "").strip()
         time.sleep(delay)
-        results = requests.get(nextpage, headers=HEADERS)
+        results = requests.get(nextpage, headers=get_headers())
         all_results.extend(results.json())
 
     return all_results
@@ -71,47 +69,64 @@ class TeamSyncer(Resource):
 
     def post(self):  # pylint: disable=no-self-use
         "POST method"
-        forwarded_for = u'{}'.format(request.headers.get('X-Forwarded-For'))
-        ip = request.remote_addr # pylint: disable=invalid-name
+        forwarded_for = u"{}".format(request.headers.get("X-Forwarded-For"))
+        print("forwarded_for is {}".format(forwarded_for))
+        ip = request.remote_addr  # pylint: disable=invalid-name
         obj = request.get_json()
 
         # check to make sure request originates from localhost or a github ip
         # ideally we should be using a secret token as well.
-        if ip == "127.0.0.1":
-            pass # ok, we are developing
-        elif forwarded_for:
+        # if ip == "127.0.0.1": # FIXME UNCOMMENT THIS BIT
+        #     print("we are localhost")
+            # pass # ok, we are developing TODO FIXME uncomment
+        if forwarded_for:
             addrs = forwarded_for.split(", ")
 
-            whitelist = requests.get(str(GITHUB.meta)).json()['hooks']
+            whitelist = requests.get(str(GITHUB.meta)).json()["hooks"]
 
             for addr in addrs:
+                print("addr is {}".format(addr))
                 client_ip_address = ip_address(addr)
 
                 for valid_ip in whitelist:
                     if client_ip_address in ip_network(valid_ip):
+                        print(
+                            "client ip address {} is in ip_network {}".format(
+                                client_ip_address, valid_ip
+                            )
+                        )
                         break
-                else:
-                    break
-            else:
-                return {'message': 'u r not authorized'}
+                else: # if there was no break
+                    print(
+                        "no valid ip in whitelist segment {} for {}".format(
+                            whitelist, client_ip_address
+                        )
+                    )
+                    # break
+            else: # if there was no break
+                print("no valid ip in any of the provided ips")
+                return {"message": "u r not authorized"}
         else:
-            return {'message': 'u r not authorized'}
-        if not 'action' in obj:
-            if 'zen' in obj:
-                return {'message': 'how zen of you'}
-            return {'message': 'this is not an event we care about'}
+            print("forwarded_for is not set, exiting")
+            return {"message": "u r not authorized"}
+        if True:
+            return {'message': 'premature exit FIXME'}
+        if not "action" in obj:
+            if "zen" in obj:
+                return {"message": "how zen of you"}
+            return {"message": "this is not an event we care about"}
 
         if not obj["action"] in ["member_added", "member_removed"]:
             return {"message": "ignoring the {} action".format(obj["action"])}
         adding = obj["action"] == "member_added"
         login = obj["membership"]["user"]["login"]
-        os.getenv("GITHUB_ORG")
         teams = get_paginated_results(str(GITHUB.orgs(os.getenv("GITHUB_ORG")).teams))
         team = None
         for ateam in teams:
             if ateam["name"] == os.getenv("GITHUB_TEAM"):
                 team = ateam
-        if not team:
+                break
+        else:
             return {"message": "there is no {} team.".format(os.getenv("GITHUB_TEAM"))}
         members = get_paginated_results(str(GITHUB.teams(team["id"]).members))
         membernames = [x["login"] for x in members]
@@ -124,12 +139,14 @@ class TeamSyncer(Resource):
                 }
             else:
                 requests.put(
-                    str(GITHUB.teams(team["id"]).memberships(login)), headers=HEADERS
+                    str(GITHUB.teams(team["id"]).memberships(login)),
+                    headers=get_headers(),
                 )
         else:
             if login in membernames:
                 requests.delete(
-                    str(GITHUB.teams(team["id"]).memberships(login)), headers=HEADERS
+                    str(GITHUB.teams(team["id"]).memberships(login)),
+                    headers=get_headers(),
                 )
             else:
                 return {
@@ -146,4 +163,4 @@ API.add_resource(TeamSyncer, "/")
 if __name__ == "__main__":
     verify_env_vars()
     setup_headers()
-    APP.run(debug=False)  # TODO Change debug to True for testing.
+    APP.run(debug=True)  # FIXME  TODO Change debug to True for testing.
